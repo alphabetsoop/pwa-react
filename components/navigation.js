81bf863859486c961ea81ddd6ba323fda3304f2e
Victor J. Bezak III <vbezak@razoyo.com>
2021-02-14 11:45:32 -0600

    Removed public/sw.* from git tracking

--- components/navigation.js
+++ components/navigation.js
@@ -1,36 +1,31 @@
 import React from 'react'
 import Link from 'next/link'
 
-import { AppBar, BottomNavigation } from '@material-ui/core';
+import {
+  AppBar,
+  Toolbar,
+  BottomNavigation
+} from '@material-ui/core';
 
 const navigation = () => {
   return (
     <nav>
       <AppBar position="fixed">
-        <Toolbar></Toolbar>
-      </AppBar>
-      <ul>
-        <li>
+        <Toolbar>
           <Link href="/">
             <a>Home</a>
           </Link>
-        </li>
-        <li>
           <Link href="/information">
             <a>Information</a>
           </Link>
-        </li>
-        <li>
           <Link href="/locations">
             <a>Locations</a>
           </Link>
-        </li>
-        <li>
           <Link href="/challenges">
             <a>Challenges</a>
           </Link>
-        </li>
-      </ul>
+        </Toolbar>
+      </AppBar>
     </nav>
     // Bottom Nav
   )
