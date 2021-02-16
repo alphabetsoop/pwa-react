ad8121e69328e1d197b30a21e644cd603f4700a0
Kailash S <kailash.s.2012@gmail.com>
2021-02-16 17:06:18 -0600

    Fix styling

--- pages/challenges.js
+++ pages/challenges.js
@@ -5,7 +5,54 @@ const challenges = () => {
   return (
     <div>
       <h1>Challenges v2 </h1>
-      <ChallengeCard />
+
+      <ChallengeCard 
+        about="Recycle" 
+        detail="Recycling is a vital first step to protecting the planet. " 
+        reward="N/A" 
+        current={3} 
+        total={14} 
+      />
+
+      <ChallengeCard 
+        about="Reusable Water" 
+        detail="Refill your reusable water bottle 5 times." 
+        reward="N/A" 
+        current={4} 
+        total={5} 
+      />
+
+      <ChallengeCard 
+        about="Shower" 
+        detail="Take a five minute shower 7 times." 
+        reward="N/A" 
+        current={2} 
+        total={7} 
+      />
+
+      <ChallengeCard 
+        about="Recycle" 
+        detail="Recycling is a vital first step to protecting the planet. " 
+        reward="N/A" 
+        current={3} 
+        total={14} 
+      />
+
+      <ChallengeCard 
+        about="Reusable Water" 
+        detail="Refill your reusable water bottle 5 times." 
+        reward="N/A" 
+        current={4} 
+        total={5} 
+      />
+
+      <ChallengeCard 
+        about="Shower" 
+        detail="Take a five minute shower 7 times." 
+        reward="N/A" 
+        current={2} 
+        total={7} 
+      />
     </div>
   )
 }
