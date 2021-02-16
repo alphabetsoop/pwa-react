f0e25a03ee4481177ca1434ed9c389d88a3b2437
Kailash S <kailash.s.2012@gmail.com>
2021-02-16 17:11:51 -0600

    Fix styling again

--- pages/challenges.js
+++ pages/challenges.js
@@ -31,27 +31,27 @@ const challenges = () => {
       />
 
       <ChallengeCard 
-        about="Recycle" 
-        detail="Recycling is a vital first step to protecting the planet. " 
-        reward="N/A" 
-        current={3} 
+        about="Another challenge" 
+        detail="Details about the challenge" 
+        reward="Rewards for completion" 
+        current={8} 
         total={14} 
       />
 
       <ChallengeCard 
-        about="Reusable Water" 
-        detail="Refill your reusable water bottle 5 times." 
-        reward="N/A" 
-        current={4} 
+        about="Another challenge" 
+        detail="Details about the challenge" 
+        reward="Rewards for completion" 
+        current={2} 
         total={5} 
       />
 
       <ChallengeCard 
-        about="Shower" 
-        detail="Take a five minute shower 7 times." 
-        reward="N/A" 
-        current={2} 
-        total={7} 
+        about="Another challenge" 
+        detail="Details about the challenge" 
+        reward="Rewards for completion" 
+        current={15} 
+        total={90} 
       />
     </div>
   )
